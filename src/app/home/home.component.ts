49e5078e956eff4ecdbd1418603a5cd1ec6590a8
Samuel Henrique Oliveira da Silva <samuel.oliveira@ibm.com>
2019-05-25 23:04:45 -0300

    More work and inprovements

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ScrollDispatcher, CdkScrollable } from '@angular/cdk/overlay';
+import { CdkScrollable, ScrollDispatcher } from '@angular/cdk/overlay';
+import { Component, NgZone, OnInit } from '@angular/core';
 import { filter } from 'rxjs/operators';
 
 @Component({
@@ -15,29 +15,39 @@ export class HomeComponent implements OnInit {
 
   selectedTab = '';
 
-  constructor(private scroll: ScrollDispatcher) {
-    scroll.scrolled(500).pipe(
+  constructor(scroll: ScrollDispatcher, private zone: NgZone) {
+    scroll.scrolled(50).pipe(
       filter((x): x is CdkScrollable => !!x && x instanceof CdkScrollable)
     ).subscribe(this.updateSelectedTab.bind(this));
   }
 
   updateSelectedTab(ev: CdkScrollable) {
-    const current = this.navLinks.map(x => document.getElementById(x.path))
+    const current = this.navLinks.map(x => document.querySelector(x.path))
       .filter((x): x is HTMLElement => x != null)
       .find(wrk => {
-        const bounding = wrk.getBoundingClientRect();
+        const b = wrk.getBoundingClientRect();
+        if (b instanceof DOMRect) {
+          return (
+            b.y > 0 &&
+            b.x > 0 &&
+            b.x < window.innerWidth &&
+            b.y < window.innerHeight
+          );
+        }
         return (
-          bounding.top >= 0 &&
-          bounding.left >= 0 &&
-          bounding.right <= (window.innerWidth) &&
-          bounding.bottom <= (window.innerHeight)
+          (b.top >= 0 || b.top + b.height >= 0) &&
+          b.left >= 0 &&
+          b.right <= window.innerWidth &&
+          b.bottom - b.height <= window.innerHeight
         );
       });
+    this.zone.run(() => {
       if (current != null) {
         this.selectedTab = `#${current.id}`;
       } else {
         this.selectedTab = '';
       }
+    });
   }
 
   ngOnInit() { }
